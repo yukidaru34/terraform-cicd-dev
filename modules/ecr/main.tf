cc90c3120a765fdf9bfe6fadbd379c59a4f3d36a
Aikawa Yuki <aikawa.yuki@tis.co.jp>
2024-04-26 06:46:28 +0000

    main 修正

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -77,3 +77,16 @@ resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
     }
   )
 }
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.name
+  cidr_block = "10.0.0.0/18"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
