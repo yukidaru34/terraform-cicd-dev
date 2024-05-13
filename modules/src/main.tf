383f28ea0394fe910f755569c57d3714d9d8cba2
Aikawa Yuki <aikawa.yuki@tis.co.jp>
2024-05-13 06:41:54 +0000

    タスク定義と関連の設定を追加しました。

--- modules/src/main.tf
+++ modules/src/main.tf
@@ -181,3 +181,70 @@ resource "aws_security_group_rule" "ecs" {
   # 同一VPC内からのアクセスのみ許可
   cidr_blocks = ["10.0.0.0/16"]
 }
+
+#　タスク定義
+resource "aws_ecs_task_definition" "main" {
+  family = "etsk-dev-I231-soumu"
+  network_mode = "awsvpc"
+  execution_role_arn = aws_iam_role.ecs_role.arn
+  requires_compatibilities = [ "FARGATE" ]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "esvc-dev-I231",
+      "image": "esvc-dev-I231:latest",
+      "portMappings": [
+        {
+            "name": "fargatetest-nginx-80-tcp",
+            "containerPort": 80,
+            "hostPort": 80,
+            "protocol": "tcp",
+            "appProtocol": "http"
+        }
+        ],
+      "runtimePlatform": {
+        "cpuArchitecture": "X86_64",
+        "operatingSystemFamily": "LINUX"
+      },
+      "cpu": 256,
+      "memory": 512
+    }
+  ]
+  DEFINITION
+}
+
+resource "aws_iam_role" "ecs_role" {
+  name = "ecs_role"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ecs-tasks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+  EOF
+}
+
+#　ECSサービス
+resource "aws_ecs_service" "main" {
+  name = "esvc-dev-I231"
+  cluster = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+  network_configuration {
+    assign_public_ip = false
+    subnets          = [data.aws_subnet.subnet_a.id, data.aws_subnet.subnet_b.id,data.aws_subnet.subnet_c.id]
+    security_groups  = [aws_security_group.ecs.id]
+  }
+}
