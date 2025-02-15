16a2cf6478926fe84ca687ed1d411aae88a46a13
Yuki <aikawa.yuki@tis.co.jp>
2025-02-15 09:44:09 +0000

    chore: apply済み

--- ecs/vpc/main.tf
+++ ecs/vpc/main.tf
@@ -1,34 +1,39 @@
-resource "aws_subnet" "public_a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.0/28"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public_b" {
+resource "aws_subnet" "public_a" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.4/28"
+  cidr_block = "10.0.0.0/26"
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "public_d" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.64/26"
+  availability_zone = "ap-northeast-1d"
 }
 
 resource "aws_subnet" "public_c" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.8/28"
+  cidr_block = "10.0.0.128/26"
+  availability_zone = "ap-northeast-1c"
 }
 
 resource "aws_subnet" "private_a" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.0/28"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
 }
 
-resource "aws_subnet" "private_b" {
+resource "aws_subnet" "private_d" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.4/28"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1d"
 }
 
 resource "aws_subnet" "private_c" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.8/28"
-}
-
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/26"
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-1c"
 }
