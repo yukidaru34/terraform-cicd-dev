793340a0834f269fcd987f3d658aa300c5838aac
Yuki <aikawa.yuki@tis.co.jp>
2025-02-15 09:47:37 +0000

    fea: rdsのリソース作成用ファイルの作成。

---
+++ rds/main.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "main" {
+    allocated_storage    = 10
+    db_name              = "test_main"
+    engine               = "postgres"
+    engine_version       = "16.3"
+    instance_class       = "db.t4g.micro"
+    username             = "M@ster"
+    password             = "P@ssw0rd"
+    skip_final_snapshot  = true
+}
